20b90d4489e8671a5a778c72b171efbffcd4fee3
Exitling <mattimueller9@gmail.com>
2024-04-27 16:51:39 +0200

    as

---
+++ src/main/java/net/exitstudios/lbd/LBDClient.java
@@ -0,0 +1,12 @@
+package net.exitstudios.lbd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LBDClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
---
+++ src/main/java/net/exitstudios/lbd/LBDDataGenerator.java
@@ -0,0 +1,13 @@
+package net.exitstudios.lbd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LBDDataGenerator implements DataGeneratorEntrypoint {
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+
+}
